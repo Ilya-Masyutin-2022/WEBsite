a7949591137043c0f8e3c55e0facf60b5164afa1
Ilya-Masyutin-2022 <118431601+Ilya-Masyutin-2022@users.noreply.github.com>
2024-05-06 18:21:24 +0700

    Отображение полей формы

--- hardcorebandssite/bands/forms.py
+++ hardcorebandssite/bands/forms.py
@@ -3,9 +3,11 @@ from .models import Category, Frontman
 
 
 class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255)
-    slug = forms.SlugField(max_length=255)
-    content = forms.CharField(widget=forms.Textarea(), required=False)
-    is_published = forms.BooleanField(required=False)
-    cat = forms.ModelChoiceField(queryset=Category.objects.all())
-    frontman = forms.ModelChoiceField(queryset=Frontman.objects.all(), required=False)
+    title = forms.CharField(max_length=255, label="Заголовок")
+    slug = forms.SlugField(max_length=255, label="URL")
+    content = forms.CharField(widget=forms.Textarea(), required=False, label="Контент")
+    is_published = forms.BooleanField(required=False, label="Статус", initial=True)
+    cat = forms.ModelChoiceField(queryset=Category.objects.all(),
+                                 label="Категории", empty_label="Категория не выбрана")
+    frontman = forms.ModelChoiceField(queryset=Frontman.objects.all(), required=False,
+                                      label="Лидер группы", empty_label="Без лидера")
