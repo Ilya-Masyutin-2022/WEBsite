cb9c890b3a7aad2bc3c590a1085a2fa8932d216c
Ilya-Masyutin-2022 <118431601+Ilya-Masyutin-2022@users.noreply.github.com>
2024-05-08 21:52:45 +0700

    Формы, связанные с моделями

--- hardcorebandssite/bands/views.py
+++ hardcorebandssite/bands/views.py
@@ -74,11 +74,8 @@ def addpage(request):
     if request.method == 'POST':
         form = AddPostForm(request.POST)
         if form.is_valid():
-            try:
-                Bands.objects.create(**form.cleaned_data)
-                return redirect('home')
-            except:
-                form.add_error(None, 'Ошибка добавления поста')
+            form.save()
+            return redirect('home')
     else:
         form = AddPostForm()
     return render(request, 'bands/addpage.html',
