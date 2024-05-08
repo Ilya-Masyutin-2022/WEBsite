36d98137c897e881c6b33025ceadfb323fef023a
Ilya-Masyutin-2022 <118431601+Ilya-Masyutin-2022@users.noreply.github.com>
2024-05-08 21:52:53 +0700

    Формы, связанные с моделями

--- hardcorebandssite/bands/forms.py
+++ hardcorebandssite/bands/forms.py
@@ -2,32 +2,43 @@ from django import forms
 from django.core.exceptions import ValidationError
 from django.utils.deconstruct import deconstructible
 
-from .models import Category, Frontman
+from .models import Category, Frontman, Bands
 from django.core.validators import MinLengthValidator, MaxLengthValidator
 
 
-class AddPostForm(forms.Form):
+class AddPostForm(forms.ModelForm):
+    cat = forms.ModelChoiceField(queryset=Category.objects.all(),
+                                 empty_label="Категория не выбрана", label="Категории")
+    frontman = forms.ModelChoiceField(queryset=Frontman.objects.all(),
+                                      required=False, empty_label="Без лидера", label="Лидер группы")
+
+    class Meta:
+        model = Bands
+        fields = ['title', 'slug', 'content',
+                  'is_published', 'cat', 'frontman', 'tags']
+        labels = {'slug': 'URL'}
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-input'}),
+            'content': forms.Textarea(attrs={'cols': 60, 'rows': 10}),
+        }
+
     def clean_title(self):
         title = self.cleaned_data['title']
-        ALLOWED_CHARS = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯабвгдеёжзийклмнопрстуфхцчшщбыъэюя0123456789 - "
-
+        ALLOWED_CHARS = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯабвгдеёжзийклмнопрстуфхцчшщбыъэюя0123456789- "
         if not (set(title) <= set(ALLOWED_CHARS)):
             raise ValidationError("Должны быть только русские символы, дефис и пробел.")
         return title
 
     title = forms.CharField(max_length=255,
                             min_length=5, label="Заголовок",
-                            widget=forms.TextInput(attrs={'class': 'form - input'}),
-                            validators=[clean_title()],
-                            error_messages={'min_length': 'Слишком короткий заголовок',
-                                            'required': 'Без заголовка - никак'})
+                            widget=forms.TextInput(attrs={'class': 'form-input'}),
+                            error_messages={
+                                'min_length': 'Слишком короткий заголовок',
+                                'required': 'Без заголовка - никак',
+                            })
+
     slug = forms.SlugField(max_length=255, label="URL",
                            validators=[MinLengthValidator(5, message="Минимум 5 символов"),
                                        MaxLengthValidator(100, message="Максимум 100 символов")])
     content = forms.CharField(widget=forms.Textarea(attrs={'cols': 50, 'rows': 5}), required=False, label="Контент")
     is_published = forms.BooleanField(required=False, label="Статус", initial=True)
-    cat = forms.ModelChoiceField(queryset=Category.objects.all(),
-                                 label="Категории", empty_label="Категория не выбрана")
-    frontman = forms.ModelChoiceField(queryset=Frontman.objects.all(), required=False, 
-                                      label="Лидер группы", empty_label="Без лидера")
-        
