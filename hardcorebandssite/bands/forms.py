c739b2910586e97b25a9fc6eb924b81375b73454
Ilya-Masyutin-2022 <118431601+Ilya-Masyutin-2022@users.noreply.github.com>
2024-05-08 22:08:10 +0700

    Создание собственных валидаторов формы

--- hardcorebandssite/bands/forms.py
+++ hardcorebandssite/bands/forms.py
@@ -24,9 +24,8 @@ class AddPostForm(forms.ModelForm):
 
     def clean_title(self):
         title = self.cleaned_data['title']
-        ALLOWED_CHARS = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯабвгдеёжзийклмнопрстуфхцчшщбыъэюя0123456789- "
-        if not (set(title) <= set(ALLOWED_CHARS)):
-            raise ValidationError("Должны быть только русские символы, дефис и пробел.")
+        if len(title) > 50:
+            raise ValidationError('Длина превышает 50 символов')
         return title
 
     title = forms.CharField(max_length=255,
