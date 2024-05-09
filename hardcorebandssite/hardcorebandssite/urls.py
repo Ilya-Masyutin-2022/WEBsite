dc60039bf9c38626aa0673ef603899cb8b2c5123
Ilya-Masyutin-2022 <118431601+Ilya-Masyutin-2022@users.noreply.github.com>
2024-05-09 20:37:41 +0700

    Отображение загруженных изображений в HTML-документе

--- hardcorebandssite/hardcorebandssite/urls.py
+++ hardcorebandssite/hardcorebandssite/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 from bands.views import *
+from hardcorebandssite import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,5 +29,11 @@ urlpatterns = [
 
 admin.site.site_header = "Панель администрирования"
 admin.site.index_title = "Популярные core-группы"
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+
 
 handler404 = pageNotFound
