42b35536e75c8d8069da2e84c9b2ba6632b2b6d6
Ilya-Masyutin-2022 <118431601+Ilya-Masyutin-2022@users.noreply.github.com>
2024-06-03 13:11:22 +0700

    Улучшение формы авторизации

--- hardcorebandssite/users/views.py
+++ hardcorebandssite/users/views.py
@@ -9,7 +9,7 @@ from users.forms import LoginUserForm
 
 
 class LoginUser(LoginView):
-    form_class = AuthenticationForm
+    form_class = LoginUserForm
     template_name = 'users/login.html'
     extra_context = {'title': "Авторизация"}
 
